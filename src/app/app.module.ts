a8c0e0aed32bc4458b7822d798a4ce0e61bd3fd6
lsegovia <lauro.segovia@konecta.com.py>
2021-08-27 22:42:55 -0400

    disenho

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app.routing.module';
 
+//componentes
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { PagesComponent } from './pages/pages.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BreadcrumbsComponent,
+    SidebarComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Grafica1Component,
+    NopagefoundComponent,
+    LoginComponent,
+    RegisterComponent,
+    PagesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
